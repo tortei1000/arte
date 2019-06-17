24d53bb9a770c6145b31e39d065e291d84ae031a
tortei1000 <tiagomaffei@gmail.com>
2019-06-17 13:47:49 -0600

    wokring on praias

--- artesMari/src/pages/index.js
+++ artesMari/src/pages/index.js
@@ -11,6 +11,7 @@ import pic04 from '../assets/images/pic04.jpg'
 import pic05 from '../assets/images/pic05.jpg'
 import pic06 from '../assets/images/pic06.jpg'
 import marinha from '../assets/images/marinha.jpg'
+import ferrugem from '../assets/images/ferrugem.jpg'
 
 class HomeIndex extends React.Component {
     render() {
@@ -33,16 +34,16 @@ class HomeIndex extends React.Component {
                         <article style={{backgroundImage: `url(${marinha})`}}>
                             <header className="major">
                                 <h3>Marinha</h3>
-                                <p>2018</p>
-                            </header>
-                            <Link to="/marinha" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic02})`}}>
-                            <header className="major">
-                                <h3>Tempus</h3>
-                                <p>feugiat amet tempus</p>
+                                <p>2018-2019</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
+                        </article>
+                        <article style={{backgroundImage: `url(${ferrugem})`}}>
+                            <header className="major">
+                                <h3>Praias</h3>
+                                <p>2018-2019</p>
+                            </header>
+                            <Link to="/praias" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic03})`}}>
                             <header className="major">
--- artesMari/src/pages/praias.js
+++ artesMari/src/pages/praias.js
@@ -2,41 +2,39 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
-import BannerLanding from '../components/BannerLanding'
-import marinha from '../assets/images/marinha.jpg'
-import marinhaII from '../assets/images/marinhaII.jpg'
-import pic09 from '../assets/images/pic09.jpg'
-import pic10 from '../assets/images/pic10.jpg'
 
-const Landing = (props) => (
+import ferrugem from '../assets/images/ferrugem.jpg'
+import marinhaII from '../assets/images/marinhaII.jpg'
+
+const Praias = (props) => (
     <Layout>
         <Helmet>
             <title>Marinha</title>
             <meta name="description" content="Landing Page" />
         </Helmet>
 
-        {/* <BannerLanding /> */}
+       
 
         <div id="main">
             <section id="one">
                 <div className="inner">
                     <header className="major">
-                        <h2>Marinha</h2>
+                        <h2>Praias</h2>
                     </header>
-                    <p>esta colecao se chama marinha porque....</p>
+                    <p>esta colecao se chama praias porque....</p>
                 </div>
             </section>
             <section id="two" className="spotlights">
                 <section>
                     <Link to="/generic" className="image">
-                        <img src={marinha} alt="" />
+                        <img src={ferrugem} alt="" />
                     </Link>
                     <div className="content">
                         <div className="inner">
                             <header className="major">
-                                <h3>Marinha</h3>
+                                <h3>Praia da Ferrugem</h3>
                             </header>
-                            <p>Pintado em 2018 a oleo. Tela 70 x 90.  Inspirado em Garopaba, SC</p>
+                            <p>Pintado em 2019 a oleo. Tela 55 x 80.  Inspirado em Praia da Ferrugem, SC</p>
                             <ul className="actions">
                                 <li><Link to="/generic" className="button">Veja Mais</Link></li>
                             </ul>
@@ -66,4 +64,4 @@ const Landing = (props) => (
     </Layout>
 )
 
-export default Landing
+export default Praias
