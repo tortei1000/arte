722a40b396d2c0fc3973335e63df0069bba48d9c
tortei1000 <tiagomaffei@gmail.com>
2019-06-18 09:45:43 -0600

    removed folders

--- artesMari/src/components/Menu.js
+++ artesMari/src/components/Menu.js
@@ -7,14 +7,14 @@ const Menu = (props) => (
         <div className="inner">
             <ul className="links">
                 <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/landing">Landing</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/generic">Generic</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/elements">Elements</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/landing">Marinha</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/praias">Praias</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/barcos">Barcos</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/onda">Onda</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/pedras">Pedras</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/amanhecer">Amanhecer/Anoitecer</Link></li>
             </ul>
-            <ul className="actions vertical">
-                <li><a href="#" className="button special fit">Get Started</a></li>
-                <li><a href="#" className="button fit">Log In</a></li>
-            </ul>
+            
         </div>
         <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>
     </nav>
